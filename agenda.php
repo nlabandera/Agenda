f8a1eaff9adb265d19aefa299a7013efaf3bcaf2
nlabandera <44570815+nlabandera@users.noreply.github.com>
2019-10-08 20:53:36 +0200

    index + cambios en agenda

--- agenda.php
+++ agenda.php
@@ -5,45 +5,58 @@ session_start();
 <html>
 <head>
 <title>agenda</title>
+<meta charset="utf-8">
 <style>
+
+	input{
+		margin-top: 5px;
+	}
     .tarjeta{
         width: 300px;
         height: 100px;
         box-shadow: 1px 1px 5px black;
         margin: 5px;
     }
+    #contacto{
+    	text-transform: uppercase;padding-top: 20px;
+    	font-weight: bold;
+    }
     p{
         text-align: center;
-        padding-top: 5px;
+       
+    }
+    legend{
+    	font-size: 20px;
     }
 </style>
 </head>
     
     <body> 
-    
+    	<h1>Hola <?php echo $_POST['propietario']?>!</h1>
         <form action="agenda.php" action="GET">
 			<fieldset>
-				<legend>Agenda</legend>
+				<legend>Añade contactos</legend>
 				<label>Nombre : </label>
                 <input id="nombre" name="nombre" type="text" placeholder="Introduce tu nombre"> <br>
 				<label>E-mail: </label>
                 <input id="email" name="email" type="text" placeholder="Introduce tu e-mail" /><br>
 				<br>
-                <input type="submit" name="submit"/>
+                <input type="submit" name="submit" value="Guardar" />
 			</fieldset>
         </form>
 
         <div>
            
              <?php 
-
             /**Comprueba si se ha pulsado el boton submit*/
             if (isset($_GET["submit"])){
-                $Sintaxis = '#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#';
+                
+                //$Sintaxis = '#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#';
                 /**Funcion strtolower para convertir la cadena a minúsculas */
                 $nombre=strtolower($_GET['nombre']);
                 $email=$_GET['email'];
-                /**Si el nombre está vacío o no lo está pero no coincide con letras: */
+                
+                /**Si el nombre está vacío o no lo está pero no coincide con letras, lanza mensaje de error */
                 if (empty($nombre) or !preg_match("/^[a-zA-Z ]*$/",$nombre)) {
                     echo "Introduce nombre";
                 }
@@ -53,14 +66,14 @@ session_start();
                     if (array_key_exists($nombre, $_SESSION['agenda']) and empty($email)) {
                         unset($_SESSION['agenda'][$nombre]);
                         foreach ($_SESSION['agenda'] as $nombre => $email) {
-                            echo '<div class="tarjeta"><p>'.$nombre.'</p><p>'.$email.'</p></div>';
+                            echo '<div class="tarjeta"><p id="contacto">'.$nombre.'</p><p id="correo">'.$email.'</p></div>';
                         }
                     }
                     
-                    /**Si el correo no coincide con la sintaxis de un correo (definida en una variable más arriba),
+                    /**Si el correo no coincide con la sintaxis de un correo,
                      * Lanza mensaje y no imprime contactos
                      */
-                    elseif (!preg_match($Sintaxis,$email)){
+                    elseif (!preg_match('#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#',$email)){
                         echo "Email erroneo";
                     }
                     /**Si el correo si coincide con la sintaxis dada, inserta los datos y muestra los contactos */
@@ -68,7 +81,7 @@ session_start();
                         $_SESSION['agenda'][$nombre]="$email";
                         
                         foreach ($_SESSION['agenda'] as $nombre => $email) {
-                            echo '<div class="tarjeta"><p id="nombre">'.$nombre.'</p><p id="correo">'.$email.'</p></div>';
+                            echo '<div class="tarjeta"><p id="contacto">'.$nombre.'</p><p id="correo">'.$email.'</p></div>';
                         }
                     }
                 }
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<form action="agenda.php" method="post">
+		<label for="text">Nombre de usuario: </label><br>
+		<input type="text" pattern="[a-zA-z]+" name="propietario" required><br><br>
+		<input type="submit">
+	</form>
+</body>
+</html>
